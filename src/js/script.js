7688a75ac7b2df4d2ebde648d68c9a6f257863f4
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-05-09 11:43:06 +0200

    add event listeners for initActions bug

--- src/js/script.js
+++ src/js/script.js
@@ -62,7 +62,7 @@
       thisProduct.getElements();
       thisProduct.initAccordion();
       thisProduct.initOrderForm();
-      thisProduct.initAmountWidget()
+      thisProduct.initAmountWidget();
       thisProduct.processOrder();
       console.log('new product', thisProduct);
     }
@@ -216,10 +216,51 @@
   class AmountWidget {
     constructor(element){
       const thisWidget = this;
+      thisWidget.getElements(element);
+      thisWidget.setValue(thisWidget.input.value);
+      thisWidget.initActions();
 
       console.log('AmountWidget', thisWidget);
       console.log('constructor arguments', element);
+
     }
+
+    getElements(element){
+      const thisWidget = this;
+
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    }
+
+    setValue(value){
+      const thisWidget = this;
+
+      const newValue = parseInt(value);
+
+      if(thisWidget.value !== newValue && !isNaN(newValue)){
+        thisWidget.value = newValue;
+      }
+      thisWidget.input.value = thisWidget.value;
+    }
+
+    initActions(){
+      const thisWidget = this;
+
+      thisWidget.input.addEventListener('change', function() {
+        thisWidget.setValue(thisWidget.input.value)
+      });
+      thisWidget.linkDecrease.addEventListener('click', function() {
+         event.preventDefault();
+         thisWidget.setValue(thisWidget.value --)
+      });
+      thisWidget.linkIncrease.addEventListener('click', function() {
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value ++)
+      });
+    }
+
 
 
 
