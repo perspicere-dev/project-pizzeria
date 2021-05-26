8514e5151ce70e9df63087b19b8fba28e73731b5
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-05-26 20:52:51 +0200

    app to files finish

--- src/js/utils.js
+++ src/js/utils.js
@@ -34,14 +34,14 @@ utils.serializeFormToObject = function(form){
   return output;
 };
 
-utils.convertDataSourceToDbJson = function(){
-  const productJson = [];
-  for(let key in dataSource.products){
-    productJson.push(Object.assign({id: key}, dataSource.products[key]));
-  }
+// utils.convertDataSourceToDbJson = function(){
+//   const productJson = [];
+//   for(let key in dataSource.products){
+//     productJson.push(Object.assign({id: key}, dataSource.products[key]));
+//   }
 
-  console.log(JSON.stringify({product: productJson, order: []}, null, '  '));
-};
+//   console.log(JSON.stringify({product: productJson, order: []}, null, '  '));
+// };
 
 Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
   return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
